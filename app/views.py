ee8d728fc97e1d88dacf94ac7e83a98efcfe243d
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-06 12:11:36 +0200

    Adding sign_up panel

--- app/views.py
+++ app/views.py
@@ -1,5 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 @app.route('/')
 def index():
     return render_template('/public/dashboard.html')
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+
+    if request.method == 'POST':
+        
+        for key, name in request.form.items():
+            print(key, name)
+        return redirect('/sign_up') # zmienic przekierowanie na sign_in
+    
+    return render_template('/public/sign_up.html')
