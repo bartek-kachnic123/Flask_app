a60a8811d89e061f4785c056b18eeb48aa65ddfb
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-08 20:14:23 +0200

    Added Users models

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Users(db.Model):
     __tablename__ = 'Users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(100))
+    username = db.Column(db.String(32))
     email = db.Column(db.String(100))
     password = db.Column(db.String(100))
 
