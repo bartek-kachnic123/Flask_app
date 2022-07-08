8d1e696893bb9051f765fe4f5df2534c925007fe
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-08 10:12:43 +0200

    Adding Users model

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class Users(db.Model):
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, user, email, passd) -> None:
+        self.username = user
+        self.email = email
+        self.password = passd
