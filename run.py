c734846cde9f2c5efb9df6fb77904cbd29e472ec
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-10 18:06:30 +0200

    Making context for flask shell

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User
+
+# Flask shell
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
 
 
 if __name__ == "__main__":
