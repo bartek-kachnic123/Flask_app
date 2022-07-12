d1563f59e754a935e894efa8cbde7084d065b2bb
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2022-07-12 17:28:50 +0200

    Added Patient model to flask shell

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import app, db
-from app.models import User
+from app.models import User, Patient
 
 # Flask shell
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Patient': Patient}
 
 
 if __name__ == "__main__":
